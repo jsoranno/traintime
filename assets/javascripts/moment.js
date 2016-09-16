d75db3f43f0bed90e4428ea0f0ec1f54cc27eefc
Juanita Soranno <juanitasoranno@gmail.com>
2016-09-16 19:29:48 -0400

    New logic added

--- assets/javascripts/moment.js
+++ assets/javascripts/moment.js
@@ -1,9 +1,31 @@
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCxOaTKird9qLx2DzTh0-at1YwFUlUsimc",
-    authDomain: "chug-traintime.firebaseapp.com",
-    databaseURL: "https://chug-traintime.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "105595551581"
-  };
-  firebase.initializeApp(config);
+var config = {
+	apiKey: "AIzaSyCxOaTKird9qLx2DzTh0-at1YwFUlUsimc",
+	authDomain: "chug-traintime.firebaseapp.com",
+	databaseURL: "https://chug-traintime.firebaseio.com",
+	storageBucket: "",
+	messagingSenderId: "105595551581"
+}; // end config
+	firebase.initializeApp(config);
+
+//initial variables
+var database = firebase.database();
+// var tableArray {
+// 	name: "",
+// 	destination: "",
+// 	frequency: 0,
+// 	next: 0,
+// 	minutes: 0,
+// };
+var name = "";
+var destination = "";
+var frequency = 0;
+var next = 0;
+var minutes = 0;
+
+// Add a new train click function
+$(".new").on("click", function(){
+	var newTrain = $("#newName").val().trim();
+	var newDest = $("#newDestination").val().trim();
+	var new
+});
