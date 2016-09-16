337edeaa550296f80e8785f2683ba09ef5a068b1
Juanita Soranno <juanitasoranno@gmail.com>
2016-09-15 23:16:01 -0400

    Added initial files

---
+++ assets/javascripts/moment.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCxOaTKird9qLx2DzTh0-at1YwFUlUsimc",
+    authDomain: "chug-traintime.firebaseapp.com",
+    databaseURL: "https://chug-traintime.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "105595551581"
+  };
+  firebase.initializeApp(config);
