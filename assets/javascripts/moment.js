cdf5eda0074a5a6eb0f518e78031ce7fbb6a0a3f
Juanita Soranno <juanitasoranno@gmail.com>
2016-09-19 20:13:58 -0400

    Added click function

--- assets/javascripts/moment.js
+++ assets/javascripts/moment.js
@@ -10,13 +10,6 @@ var config = {
 
 //initial variables
 var database = firebase.database();
-// var tableArray {
-// 	name: "",
-// 	destination: "",
-// 	frequency: 0,
-// 	next: 0,
-// 	minutes: 0,
-// };
 var name = "";
 var destination = "";
 var frequency = 0;
@@ -24,14 +17,34 @@ var next = 0;
 var minutes = 0;
 
 // Add a new train click function
-$(".new").on("click", function(){
-	var newTrain = $("#newName").val().trim();
+$('.btn').on('click', function(){
+	console.log("Submit Clicked")
+	var newName = $("#newName").val().trim();
 	var newDest = $("#newDestination").val().trim();
 	var newTime = $("#newTime").val().trim();
 	var newFreq = $("#newFreq").val().trim();
-	console.log(newTrain);
+	console.log(newName);
 	console.log(newDest);
 	console.log(newTime);
 	console.log(newFreq);
+
+	var newTrain = {
+	name:  newName,
+	dest: newDest,
+	start: newTime,
+	freq: newFreq
+	}//close newTrain
+
+	database.ref().push(newTrain);
+
+	$("#newName").val("");
+	$("#newDestination").val("");
+	$("#newTime").val("");
+	$("#newFreq").val("");
+
+	return false;
 });
 //add error message to say, "oh bumpers!"
+
+
+
